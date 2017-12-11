337952ef2065a809e34023898873f49c496f7651
marco bakels <marcobakels@live.nl>
2017-12-11 09:23:46 +0100

    Start of project and added a player ship

--- start/main.cpp
+++ start/main.cpp
@@ -15,22 +15,21 @@
  */
 #include <rt2d/core.h>
 
-#include "myscene.h"
+#include "GameScene.h"
 
 /// @brief main entry point
-int main( void )
-{
+int main(void){
 	// Core instance
 	Core core;
 
 	// Scene01
-	MyScene* myscene = new MyScene(); // create Scene on the heap
-	while(myscene->isRunning()) { // check status of Scene every frame
-		core.run(myscene); // update and render the current scene
+	GameScene* game = new GameScene(); // create Scene on the heap
+	while(game->isRunning()) { // check status of Scene every frame
+		core.run(game); // update and render the current scene
 		core.showFrameRate(5); // show framerate in output every n seconds
 	}
 	//core.cleanup(); // cleanup ResourceManager (Textures + Meshes, but not Shaders)
-	delete myscene; // delete Scene and everything in it from the heap to make space for next Scene
+	delete game; // delete Scene and everything in it from the heap to make space for next Scene
 
 	// No need to explicitly clean up the core.
 	// As a local var, core will go out of scope and destroy Renderer->ResourceManager.
