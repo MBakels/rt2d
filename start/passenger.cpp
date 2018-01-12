01f97467f0f37c0ea1767036faf2cc59d0fa5233
marco bakels <marcobakels@live.nl>
2018-01-12 13:51:30 +0100

    Added passenger class

---
+++ start/passenger.cpp
@@ -0,0 +1,19 @@
+/**
+* This class describes the Passenger behavior.
+*
+* Copyright 2015 Marco Bakels <marcobakels@live.nl>
+*/
+
+#include "passenger.h"
+
+Passenger::Passenger(Body* originPlanet) {
+	
+}
+
+Passenger::~Passenger() {
+
+}
+
+void Passenger::SetRandomDestination(Body* originPlanet) {
+
+}
---
+++ start/passenger.h
@@ -0,0 +1,32 @@
+/**
+* Copyright 2015 Marco Bakels <marcobakels@live.nl>
+*
+* @file passenger.h
+*
+* @brief description of the Passenger behavior.
+*/
+
+#ifndef PASSENGER_H
+#define PASSENGER_H
+
+#include <string>
+
+#include "body.h"
+
+/// @brief The SpaceShip class handels the spaceship movement and keeps a list of passengers.
+class Passenger {
+public:
+	/// @brief Constructor
+	Passenger(Body* originPlanet);
+	/// @brief Destructor
+	virtual ~Passenger();
+
+private:
+	std::string destination;
+
+	std::string GetDestination() { return destination; };
+
+	void SetRandomDestination(Body* originPlanet);
+};
+
+#endif /* PASSENGER_H */
